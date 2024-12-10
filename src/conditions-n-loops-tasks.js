49d01f1dcba2e27e37d3e1c5cc6d53a7871f21a4
Daria Leskina <dleskinaleskina@yandex.ru>
2024-12-10 22:59:28 +0300

    feat: solve getMaxNumber task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,19 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let x = 0;
+  if (a === b && a === c) {
+    x = a;
+  }
+  if (a > b && a > c) {
+    x = a;
+  } else if (b > a && b > c) {
+    x = b;
+  } else if (c > a && c > b) {
+    x = c;
+  }
+  return x;
 }
 
 /**
